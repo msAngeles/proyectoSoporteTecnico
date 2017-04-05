f12345f2430f64366bf91f432c91e5d43dd2200a
Alan Martinez <alanmartinez70@hotmail.com>
2017-04-05 12:37:05 -0500

    archivos agregados:conexion,login,logout,panel,recuperar,validar

--- validar.php
+++ validar.php
@@ -4,7 +4,7 @@
 		if(isset($_POST['btn_login'])){
 			$usuario = $_POST['usuario'];
 			$password = $_POST['password'];
-			$log = mysql_query("SELECT * FROM usuario WHERE usuario='$usuario' AND pass='$password'");
+			$log = mysql_query("SELECT * FROM usuario WHERE usuario='$usuario' AND pass='$password'"); //pass blob
 			if (mysql_num_rows($log)>0) {
 				$row = mysql_fetch_array($log);
 				$_SESSION["usuario"] = $row['usuario'];
