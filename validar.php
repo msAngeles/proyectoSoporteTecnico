f227029378e4d4731b6613001a584f6eaaa9a5be
Miriam Santiago <arkangel_nissi@hotmail.com>
2017-04-05 17:41:18 -0500

    modificacion del login, faltan validaciones, miercoles 4 de abril de 2017

--- validar.php
+++ validar.php
@@ -5,12 +5,13 @@
 		if(isset($_POST['btn_login'])){
 			$usuario = $_POST['usuario'];
 			$password = $_POST['password'];
-			$log = mysql_query("SELECT * FROM usuario WHERE nombUsuario='$usuario'"); //pass blob
+			$log = mysql_query("SELECT nombUsuario,AES_DECRYPT(pass,nombUsuario) FROM usuario WHERE nombUsuario='$usuario' and pass=AES_ENCRYPT('$password','$usuario')"); //pass blob
             //AES_DECRYPT(pass,nombUsuario)
 			if (mysql_num_rows($log)>0) {
 				$row = mysql_fetch_array($log);
-				$_SESSION["usuario"] = $row['nombUsuario'];
-				echo '<script> window.location="panel.php"; </script>';
+                $_SESSION["usuario"] = $row['nombUsuario'];
+				//echo '<script> window.location="panel.php"; </script>';
+                echo $row['AES_DECRYPT(pass,nombUsuario)'];
                 
 			}
 			else{
