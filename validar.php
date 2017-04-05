4bf4a68cdbc701b984038cb16e6ff1cfd0dbb96d
Alan Martinez <alanmartinez70@hotmail.com>
2017-04-05 01:49:26 -0500

    Validacion

---
+++ validar.php
@@ -0,0 +1,18 @@
+<?php
+	session_start(); 
+	include 'conexion.php';
+		if(isset($_POST['btn_login'])){
+			$usuario = $_POST['usuario'];
+			$password = $_POST['password'];
+			$log = mysql_query("SELECT * FROM usuario WHERE usuario='$usuario' AND pass='$password'");
+			if (mysql_num_rows($log)>0) {
+				$row = mysql_fetch_array($log);
+				$_SESSION["usuario"] = $row['usuario'];
+				echo '<script> window.location="panel.php"; </script>';
+			}
+			else{
+				echo '<script> alert("Usuario o contraseña incorrectos.");</script>';
+				echo '<script> window.location="login.php"; </script>';
+			}
+		}
+?>
